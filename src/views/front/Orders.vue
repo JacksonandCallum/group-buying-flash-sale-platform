0a3a2124de5b920ed52282174cd03d88e78c4f96
城北吕公 <878636660@qq.com>
2024-07-14 10:15:13 +0800

    订单中心

--- src/views/front/Orders.vue
+++ src/views/front/Orders.vue
@@ -1,10 +1,12 @@
 <template>
-  <div style="width: 80%;margin: 2px auto">
+  <div style="width: 80%;margin: 20px auto">
     <div class="box" style="margin-bottom: 5px">
-      <el-table :data="data.tableData" stripe @selection-change="handleSelectChange">
-        <el-table-column type="selection" width="30"></el-table-column>
-        <el-table-column prop="id" label="id"></el-table-column>
-        <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+      <el-table :data="data.tableData" stripe>
+        <el-table-column prop="goodsName" label="商品名称" show-overflow-tooltip fixed="left">
+          <template v-slot="scope">
+            <a :href="'/front/goodsDetail?id=' + scope.row.goodsId" target="_blank">{{ scope.row.goodsName }}</a>
+          </template>
+        </el-table-column>
         <el-table-column prop="goodsImg" label="商品图片">
           <template #default="scope">
             <el-image v-if="scope.row.goodsImg" :src="scope.row.goodsImg" :preview-src-list="[scope.row.goodsImg]"
@@ -14,9 +16,9 @@
         <el-table-column prop="goodsPrice" label="商品单价"></el-table-column>
         <el-table-column prop="num" label="购买数量"></el-table-column>
         <el-table-column prop="total" label="商品总价"></el-table-column>
-        <el-table-column prop="orderNo" label="订单编号"></el-table-column>
-        <el-table-column prop="payNo" label="支付编号"></el-table-column>
-        <el-table-column prop="payTime" label="支付时间"></el-table-column>
+        <el-table-column prop="orderNo" label="订单编号" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="payNo" label="支付编号" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="payTime" label="支付时间" show-overflow-tooltip></el-table-column>
         <el-table-column prop="userName" label="下单人"></el-table-column>
         <el-table-column prop="createTime" label="创建时间" show-overflow-tooltip></el-table-column>
         <el-table-column prop="status" label="订单状态">
@@ -38,16 +40,20 @@
             <span v-if="scope.row.type === 'FLASH'">秒杀</span>
           </template>
         </el-table-column>
-        <el-table-column label="操作" header-align="center" width="260">
+        <el-table-column label="操作" header-align="center" width="260" fixed="right">
           <template #default="scope">
-            <el-button plain type="primary" @click="update(scope.row)">发货</el-button>
+            <el-button plain type="primary" @click="update(scope.row)"
+              v-if="scope.row.status === 'NOT_ACCEPT'">确认收货</el-button>
+            <el-button plain type="primary" @click="update(scope.row)" v-if="scope.row.status === 'DONE'">评价</el-button>
+            <el-button plain type="primary" @click="refund(scope.row.orderNo)"
+              v-if="scope.row.status === 'DONE'">退款</el-button>
             <el-button plain type="danger" @click="del(scope.row.id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
 
-    <div class="box" v-if="data.total">
+    <div v-if="data.total" style="margin: 30px auto">
       <el-pagination background layout="total,prev,pager,next" :total="data.total" v-model:current-page='data.pageNum'
         v-model:page-sizes="data.pageSize" @current-change="load"></el-pagination>
     </div>
@@ -56,20 +62,77 @@
 
 <script setup>
 import request from '@/utils/request';
-import { ElMessage } from 'element-plus';
+import { ElMessage, ElMessageBox } from 'element-plus';
 import { onMounted, reactive } from 'vue';
 import router from "@/router/index.js";
 
 const data = reactive({
-
+  user: JSON.parse(localStorage.getItem("flash-sale-user") || '{}'),
+  tableData: [],
+  pageNum: 1,
+  pageSize: 10,
+  total: 0,
 })
+
+const load = () => {
+  request
+    .get("/orders/selectPage", {
+      params: {
+        pageNum: data.pageNum,
+        pageSize: data.pageSize,
+        userId: data.user.id,
+      },
+    })
+    .then((res) => {
+      data.tableData = res.data.list
+      data.total = res.data.total
+    });
+};
+
+const del = (id) => {
+  ElMessageBox.confirm("删除后数据无法恢复，您确定删除吗？", "删除确认", { type: 'warning' }).then(response => {
+    request.delete("/orders/delete/" + id).then(res => {
+      if (res.code === '200') {
+        ElMessage.success("删除成功")
+        load()
+      } else {
+        ElMessage.error(res.msg)
+      }
+    })
+  }).catch(err => { })
+}
+
+const update = (row) => {
+  row.status = "DONE"  // 修改状态为已完成
+  request.put("/orders/update", row).then(res => {
+    if (res.code === '200') {
+      ElMessage.success("确认收货成功")
+      load()
+    } else {
+      ElMessage.error(res.msg)
+    }
+  })
+}
+
+const refund = (orderNo) => {
+  request.get("/alipay/refund?orderNo=" + orderNo).then(res => {
+    if (res.code === '200') {
+      ElMessage.success("退款成功")
+      load()
+    } else {
+      ElMessage.error(res.msg)
+    }
+  })
+}
 
 onMounted(() => {
-
-})
+  load()
+});
 </script>
 
 <style scoped>
+@import "@/assets/css/manager.css";
+
 .overflow {
   white-space: nowrap;
   overflow: hidden;
